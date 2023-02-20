1de49b36770e9eafda86d44d557d7a2d8b4e575b
Basile Bruneau <basile@bruneau.email>
2023-02-20 10:05:28 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -31,7 +31,7 @@ const findLocation = (city, countryCode) => {
   return [lat, lng];
 };
 
-const CURRENT_LOCATION = findLocation("Oslo", "NO");
+const CURRENT_LOCATION = findLocation("Stockholm", "SE");
 
 const getXYFromLatLng = ({ map, point }) => {
   const { height, width } = map.image;
